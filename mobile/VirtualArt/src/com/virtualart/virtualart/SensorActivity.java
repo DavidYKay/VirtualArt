5e227c6ad45c9e840cddab099032d60e9feef6de
David Young-Chan Kay <davidykay@gmail.com>
2010-11-13 16:42:34 -0500

    Moved sensor code into: SensorHelper. Refactored SensorActivity to use it.

--- mobile/VirtualArt/src/com/virtualart/virtualart/SensorActivity.java
+++ mobile/VirtualArt/src/com/virtualart/virtualart/SensorActivity.java
@@ -1,33 +1,20 @@
 package com.virtualart.virtualart;
 
-import com.virtualart.virtualart.render.Matrix;
-
 import android.app.Activity;
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.virtualart.virtualart.util.SensorHelper;
+import com.virtualart.virtualart.util.SensorHelper.SensorHelperListener;
 
 /**
  * Activity for testing the behavior of accelerometer/compass/GPS
  *
  */
-public class SensorActivity extends Activity implements SensorEventListener {
-
-    //Thread responsible for updating our sensor status
-    private Thread mThread;
+public class SensorActivity extends Activity implements SensorHelperListener {
     
     // Model
-
-    SensorManager sMgr;
-    Sensor sAccelerometerSensor;
-    Sensor sMagneticSensor;
-
-    private float[] mAccelerometerValues = new float[3];
-    private float[] mCompassValues       = new float[3];
+    private SensorHelper mSensorHelper;
 
     // UI
     private TextView mAccelXView;
@@ -44,17 +31,12 @@ public class SensorActivity extends Activity implements SensorEventListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //Set up sensors
-        sMgr = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sAccelerometerSensor = sMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sMagneticSensor      = sMgr.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-
-        sMgr.registerListener(this, sAccelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
-        sMgr.registerListener(this, sMagneticSensor, SensorManager.SENSOR_DELAY_GAME);
         
         //Set up view
         setContentView(R.layout.sensor);
+
+        mSensorHelper = new SensorHelper(this);
+        mSensorHelper.addListener(this);
 
         mAccelXView = (TextView)this.findViewById(R.id.accel_x_value);
         mAccelYView = (TextView)this.findViewById(R.id.accel_y_value);
@@ -64,67 +46,23 @@ public class SensorActivity extends Activity implements SensorEventListener {
         mCompassYView = (TextView)this.findViewById(R.id.compass_y_value);
         mCompassZView = (TextView)this.findViewById(R.id.compass_z_value);
         
-        
         mPitchView = (TextView)this.findViewById(R.id.pitch_value);
-
     }
+    
+    public void SensorHelperDidUpdate(SensorHelper sensorHelper) {
+        float[] accelerometerValues = sensorHelper.getAccelerometerValues();
+        float[] compassValues = sensorHelper.getCompassValues();
 
-    public void updateOrientation() {
-        float[] rotationMatrix    = new float[16];
-        float[] inclinationMatrix = new float[16];
+        mAccelXView.setText(String.format("%.1f", accelerometerValues[0]));
+        mAccelYView.setText(String.format("%.1f", accelerometerValues[1]));
+        mAccelZView.setText(String.format("%.1f", accelerometerValues[2]));
 
-        //getRotationMatrix (float[] R, float[] I, float[] gravity, float[] geomagnetic)
-        sMgr.getRotationMatrix(
-            rotationMatrix,
-            inclinationMatrix,
-            mAccelerometerValues,
-            mCompassValues
-        );
-
-        Matrix matrix = new Matrix(
-            //rotationMatrix[0][0], rotationMatrix[0][1], rotationMatrix[0][2],
-            //rotationMatrix[1][0], rotationMatrix[1][1], rotationMatrix[1][2],
-            //rotationMatrix[2][0], rotationMatrix[2][1], rotationMatrix[2][2],
-            rotationMatrix[0], rotationMatrix[1], rotationMatrix[2],
-            rotationMatrix[4], rotationMatrix[5], rotationMatrix[6],
-            rotationMatrix[8], rotationMatrix[9], rotationMatrix[10]
-        );
+        mCompassXView.setText(String.format("%.1f", compassValues[0]));
+        mCompassYView.setText(String.format("%.1f", compassValues[1]));
+        mCompassZView.setText(String.format("%.1f", compassValues[2]));
 
         mPitchView.setText(
-            matrix.toString()
-            //String.format("%.1f", sensorValues[0])
+            sensorHelper.getRotationMatrix().toString()
         );
-    }
-
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // TODO Auto-generated method stub
-    }
-
-    public void onSensorChanged(SensorEvent event) {
-        float[] sensorValues = event.values;
-        if (event.sensor == sAccelerometerSensor) {
-            mAccelerometerValues = sensorValues;
-            
-            //Show accel values
-            mAccelXView.setText(String.format("%.1f", sensorValues[0]));
-            mAccelYView.setText(String.format("%.1f", sensorValues[1]));
-            mAccelZView.setText(String.format("%.1f", sensorValues[2]));
-            
-        } else if (event.sensor == sMagneticSensor) {
-            mCompassValues = sensorValues;
-
-            //Show compass
-            for (int i =0; i < sensorValues.length; i++) { 
-                mCompassXView.setText(String.format("%.1f", sensorValues[0]));
-                mCompassYView.setText(String.format("%.1f", sensorValues[1]));
-                mCompassZView.setText(String.format("%.1f", sensorValues[2]));
-                //Log.v( "Sensor", String.format( "Sensor %d, value: %f", i, sensorValues[i]));
-            }
-        }
-
-        updateOrientation();
-
-        //SensorManager.getRotationMatrix(
-        //);
     }
 }
---
+++ mobile/VirtualArt/src/com/virtualart/virtualart/util/SensorHelper.java
@@ -0,0 +1,114 @@
+package com.virtualart.virtualart.util;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import com.virtualart.virtualart.render.Matrix;
+
+/**
+ * Class to help out with out sensor needs
+ */
+public class SensorHelper implements SensorEventListener {
+
+	public interface SensorHelperListener extends java.util.EventListener {
+		public void SensorHelperDidUpdate(SensorHelper sensorHelper);
+	}
+
+	//private Context mContext;
+    private SensorManager sMgr;
+    private Sensor sAccelerometerSensor;
+    private Sensor sMagneticSensor;
+
+    private float[] mAccelerometerValues = new float[3];
+    private float[] mCompassValues       = new float[3];
+
+	public SensorHelper(Context context) {
+		//mContext = context;
+		
+        //Set up sensors
+        sMgr = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        sAccelerometerSensor = sMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sMagneticSensor      = sMgr.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        sMgr.registerListener(this, sAccelerometerSensor, SensorManager.SENSOR_DELAY_GAME);
+        sMgr.registerListener(this, sMagneticSensor, SensorManager.SENSOR_DELAY_GAME);
+	}
+	
+	public float[] getAccelerometerValues() {
+		return mAccelerometerValues;
+	}
+	
+	public float[] getCompassValues() {
+		return mCompassValues;
+	}
+
+	public float[] getRotationMatrix() {
+        float[] rotationMatrix    = new float[16];
+        float[] inclinationMatrix = new float[16];
+
+        sMgr.getRotationMatrix(
+            rotationMatrix,
+            inclinationMatrix,
+            mAccelerometerValues,
+            mCompassValues
+        );
+
+        Matrix matrix = new Matrix(
+            //rotationMatrix[0][0], rotationMatrix[0][1], rotationMatrix[0][2],
+            //rotationMatrix[1][0], rotationMatrix[1][1], rotationMatrix[1][2],
+            //rotationMatrix[2][0], rotationMatrix[2][1], rotationMatrix[2][2],
+            rotationMatrix[0], rotationMatrix[1], rotationMatrix[2],
+            rotationMatrix[4], rotationMatrix[5], rotationMatrix[6],
+            rotationMatrix[8], rotationMatrix[9], rotationMatrix[10]
+        );
+        return rotationMatrix;
+	}
+
+    @Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // TODO Auto-generated method stub
+    }
+    
+    @Override
+	public void onSensorChanged(SensorEvent event) {
+        float[] sensorValues = event.values;
+        if (event.sensor == sAccelerometerSensor) {
+            mAccelerometerValues = sensorValues;
+        } else if (event.sensor == sMagneticSensor) {
+            mCompassValues = sensorValues;
+        }
+
+		//Notify Listeners
+        notifyListeners();
+    }
+
+
+	//Listener management
+
+	//private EventListenerList<SensorHelperListener> mEventListenerList = new EventListenerList<SensorHelperListener>();
+	private EventListenerList mEventListenerList = new EventListenerList();
+
+	public void addListener(SensorHelperListener listener) {
+		mEventListenerList.add(
+			SensorHelperListener.class,
+			listener
+		);
+	}
+	
+	public void removeListener(SensorHelperListener listener) {
+		mEventListenerList.remove(
+			SensorHelperListener.class,
+			listener
+		);
+	}
+
+	private void notifyListeners() {
+		//For each listener, call the event
+		for (SensorHelperListener listener : mEventListenerList.getListeners(SensorHelperListener.class)) {
+			listener.SensorHelperDidUpdate(this);
+		}
+	}
+}
