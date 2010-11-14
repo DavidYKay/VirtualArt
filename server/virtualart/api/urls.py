443c310fc9652535cab7762bf75bf6623516038f
dzhou <kefei.zhou@gmail.com>
2010-11-14 00:18:34 -0500

    fix url match on negative coordinates

--- server/virtualart/api/urls.py
+++ server/virtualart/api/urls.py
@@ -12,7 +12,9 @@ art_resource = Resource(ArtHandler)
 geo_resource = Resource(GeoArtHandler)
 
 urlpatterns = patterns('',
+    #url(r'^art/(?P<lat>)/(?P<lon>)/(?P<range>)$', geo_resource),
     url(r'^art/(?P<lat>\d+\.\d+)/(?P<lon>\d+\.\d+)/(?P<range>\d+\.\d+)$', geo_resource),
+    url(r'^art/(?P<lat>(-)*\d+\.\d+)/(?P<lon>(-)*\d+\.\d+)/(?P<range>\d+\.\d+)$', geo_resource),
     #url(r'^simpleart/(?P<id>\d+)$', simpleart_resource),  
     #url(r'^simpleart$', simpleart_resource),
     #url(r'^art/(?P<id>\d+)$', art_resource, { 'emitter_format': 'ext-json' }),  
