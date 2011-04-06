eb76f0a61dfc7d2ba907a15b8f29e71a698d599f
Chris Arriola <arriolac279@gmail.com>
2011-04-05 23:57:00 -0700

    Using OpenGL ES 2.0

--- mobile-jni/Artmented/jni/Android.mk
+++ mobile-jni/Artmented/jni/Android.mk
@@ -1,24 +1,24 @@
-LOCAL_PATH := $(call my-dir)
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := artmented
-
-LOCAL_CFLAGS := -DANDROID_NDK \
-                -DDISABLE_IMPORTGL
-
-LOCAL_SRC_FILES := \
-    importgl.c \
-    demo.c \
-    app-android.c \
-
-# Android automatically links the C, Math, and C++ libraries.
-# Include the libraries (in-order)
-# - OpenGL ES 1.x Library <GLES/gl.h> and <GLES/gtext.h> (Android API level 4)
-#   NOTE: OpenGL ES 2.0 containing headers <GLES2/gl2.h> and <GLES2/gl2ext.h> 
-#   is in Android API level 5. The flag for this would be '-lGLESv2
-# - Android Dynamic Linker <dlfcn.h>
-# - Android-specific Log Support <android/log.h>
-LOCAL_LDLIBS := -lGLESv1_CM -ldl -llog
+LOCAL_MODULE    := libgl2jni
+LOCAL_CFLAGS    := -Werror
+LOCAL_SRC_FILES := gl_code.cpp
+LOCAL_LDLIBS    := -llog -lGLESv2
 
 include $(BUILD_SHARED_LIBRARY)
---
+++ mobile-jni/Artmented/obj/local/armeabi/objs/gl2jni/gl_code.o.d
@@ -0,0 +1,104 @@
+/home/chris/Dropbox/workspace/VirtualArt/mobile-jni/Artmented/obj/local/armeabi/objs/gl2jni/gl_code.o: \
+ /home/chris/Dropbox/workspace/VirtualArt/mobile-jni/Artmented/jni/gl_code.cpp \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/jni.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/android/log.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/GLES2/gl2.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/GLES2/gl2platform.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/KHR/khrplatform.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/stdint.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/machine/_types.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/GLES2/gl2ext.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/stdio.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/types.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/linux/posix_types.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/linux/stddef.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/linux/compiler.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/asm/posix_types.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/asm/types.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/linux/types.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/machine/kernel.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/stdlib.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/string.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/malloc.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/alloca.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/strings.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/memory.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/math.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/limits.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/limits.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/linux/limits.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/machine/internal_types.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/machine/limits.h \
+ /home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/syslimits.h
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/jni.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/android/log.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/GLES2/gl2.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/GLES2/gl2platform.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/KHR/khrplatform.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/machine/_types.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/GLES2/gl2ext.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/stdio.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/types.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/linux/posix_types.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/linux/stddef.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/linux/compiler.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/asm/posix_types.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/asm/types.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/linux/types.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/machine/kernel.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/stdlib.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/string.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/alloca.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/strings.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/memory.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/math.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/limits.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/limits.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/linux/limits.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/machine/internal_types.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/machine/limits.h:
+
+/home/chris/local/android-ndk-r5b/platforms/android-5/arch-arm/usr/include/sys/syslimits.h:
